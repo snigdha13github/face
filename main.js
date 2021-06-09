4281132cc626d07498e0e9255f98d50f02451ec4
snigdha13github <73014313+snigdha13github@users.noreply.github.com>
2021-06-09 16:45:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,27 @@ function setup()
     c1.center();
     v1=createCapture(VIDEO);
     v1.hide();
+    real=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/BmR3O3_o5/model.json",modelLoaded)
 }
 function draw()
 {
     image(v1,0,0,600,500);
+    real.classify(v1,gotResult);
+}
+function modelLoaded()
+{
+    console.log("model has loaded");   
+}
+function gotResult(error,results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("object_name").innerHTML=results[0].label;
+        document.getElementById("object_accuracy").innerHTML=results[0].confidence.toFixed(2);
+    }
 }
